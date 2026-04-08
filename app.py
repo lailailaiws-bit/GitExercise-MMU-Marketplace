81caf29839a9574a7168ea6d97d115c2e9ab5d23
Lai Wei Shen <laiweishen2007@gmail.com>
2026-04-08 22:39:00 +0800

    changed Templates to templates

--- app.py
+++ app.py
@@ -4,7 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('base.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/acc')
+def account():
+    return render_template('account.html')
 
 if __name__ == '__main__':
     app.run(debug=True)

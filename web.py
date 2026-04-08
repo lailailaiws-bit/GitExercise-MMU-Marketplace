5b2a4ddf2297b6b20f6a7e70867388efc9b3d587
Lai Wei Shen <laiweishen2007@gmail.com>
2026-04-08 12:15:51 +0800

    css

--- web.py
+++ web.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('home.html')
+    return render_template('base.html')
 
 if __name__ == "__main__":
     app.run(debug=True)

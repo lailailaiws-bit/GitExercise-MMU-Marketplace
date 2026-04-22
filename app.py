988362c6ddb48c93404074d9e8c0b27e5ce14ac1
Caleb Hee <calebhee@gmail.com>
2026-04-22 13:25:33 +0800

    chat feature basics are almost complete

--- app.py
+++ app.py
@@ -63,7 +63,8 @@ def save_message(username, content):
 
     with open(chat_history, 'w') as f:
         json.dump(chats, f, indent=4)
-        
+       
+
 @app.route('/')
 def index():
     return render_template('base.html')
@@ -82,6 +83,7 @@ def search():
     print(f"Search query: {query}")
     return redirect(url_for('index'))
 
+
 @app.route('/chat', methods=['GET', 'POST'])
 @login_required 
 def chat():
@@ -94,6 +96,7 @@ def chat():
     # Load private messages for this specific user
     user_messages = load_messages(current_user.username)
     return render_template('chat.html', messages=user_messages)
+
 
 @app.route('/profile', methods=['GET', 'POST'])
 @login_required

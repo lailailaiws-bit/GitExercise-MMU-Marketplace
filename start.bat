1852e05c9218d43b4fea251eb232abe288f3fbd4
Lai Wei Shen <laiweishen2007@gmail.com>
2026-05-24 23:45:44 +0800

    debug

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
 echo Starting Tailwind Compiler for Flask...
-npx @tailwindcss/cli -i .\static\src\input.css -o .\static\css\output.css --watch
+.\tailwind.exe -i .\static\src\input.css -o .\static\css\output.css --watch
 pause

51d5410d78fb985122881fa135f171def7e77b2d
Haoyii <226713430+HYiii01@users.noreplay.github.com>
2026-06-24 16:45:46 +0800

    Update the database configuration and adjust the table creation location.

--- app.py
+++ app.py
@@ -14,7 +14,8 @@ app = Flask(__name__)
 
 
 app.config['SECRET_KEY'] = 'your_secret_key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] =  os.environ.get('DATABASE_URL', 'sqlite:///site.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 UPLOAD_FOLDER = 'static/css/pictures'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -484,6 +485,8 @@ def my_item():
 
     return render_template('my_item.html', item=my_products)
     
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=True)
